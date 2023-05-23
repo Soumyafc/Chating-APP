43a6225c7a294c64f9f2d91f362cad73360fe988
Soumya Ranjan Parida <soumyafc812@gmail.com>
2023-05-23 22:02:22 +0530

    second commit

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,22 @@
-function ChatsPage() {
-    return (  <>Chats...</>);
-}
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "c37a42d1-5cc7-4471-bc0f-0c62992e6ea1",
+    props.user.username,
+    props.user.secret
+  );
+
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
+};
 
 export default ChatsPage;
